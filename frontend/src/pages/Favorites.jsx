84d4984b17d3fd159d0ce3c6d82eb322a3e66ea5
Daniel Alchasov <danal315@student.liu.se>
2025-04-01 21:54:49 +0200

    adding tailwind css

--- frontend/src/pages/Favorites.jsx
+++ frontend/src/pages/Favorites.jsx
@@ -2,7 +2,7 @@ import "../css/Favorites.css";
 
 function Favorites() {
   return (
-    <div className="favorites-empty">
+    <div className="favorites-empty ">
       <h2>No Favorite Shows Yet</h2>
       <p>Add them and they'll appear there :D</p>
     </div>
